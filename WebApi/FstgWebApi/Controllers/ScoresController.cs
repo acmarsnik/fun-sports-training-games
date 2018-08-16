aa02ecac32595b91917e5eeb9862ea8d34afcbd8
Albert Marsnik <albert_marsnik@trimble.com>
2018-08-16 20:18:19 +0200

    basic WebApi functional, local configuration added to WebUi for build serve and e2e

---
+++ WebApi/FstgWebApi/Controllers/ScoresController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using FstgWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FstgWebApi.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/Scores")]
+    public class ScoresController : Controller
+    {
+        public IActionResult Get()
+        {
+            var response = new List<Score>
+            {
+                new Score { Id = 1, Value = 10 },
+                new Score { Id = 2, Value = 6 }
+            };
+
+            var output = StatusCode((int)HttpStatusCode.OK, response);
+            return output;
+        }
+    }
+}
