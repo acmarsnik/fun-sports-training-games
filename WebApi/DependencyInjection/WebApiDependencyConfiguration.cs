ec63bec8577b8685eb2b7881b8afd27d823cb431
boondoggler <cedric_bertolasio@trimble.com>
2018-10-04 10:24:36 -0600

    adding ssl and adding di project

---
+++ WebApi/DependencyInjection/WebApiDependencyConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FstgWebApi.DependencyInjection
+{
+	public class WebApiDependencyConfiguration
+    {
+		public static IContainer Configure(IServiceCollection services)
+		{
+			var builder = new ContainerBuilder();
+
+			//builder.RegisterType<OdbcAgDataWarehouseConnectionStringProvider>().As<IConnectionStringProvider>().AsSelf();
+			//builder.RegisterType<PumpStatusConverter>().As<IPumpStatusConverter>().InstancePerLifetimeScope();
+
+			//builder.RegisterType<AgDataWarehouseOdbcContext>().WithParameter(
+			//	new ResolvedParameter(
+			//		(pi, ctx) => pi.ParameterType == typeof(IConnectionFactory),
+			//		(pi, ctx) => ctx.ResolveKeyed<IConnectionFactory>("odbc-agdatawarehouse")))
+			//	.As<IOdbcDbContext>().Keyed<IOdbcDbContext>("odbc-agdatawarehouse");
+
+
+			builder.Populate(services);
+			return builder.Build();
+		}
+	}
+}
