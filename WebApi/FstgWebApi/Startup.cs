6b5c718cb22249194e993c91768be76b4cd2ecb4
Albert Marsnik <albert_marsnik@trimble.com>
2018-10-04 18:38:12 +0200

    added dependency injection stuff

--- WebApi/FstgWebApi/Startup.cs
+++ WebApi/FstgWebApi/Startup.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using FstgWebApi.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,19 +22,23 @@ namespace FstgWebApi
         }
 
         public IConfiguration Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddCors(options => options.AddPolicy("AllowAll",
                 policy => policy.AllowAnyOrigin()
                     .AllowAnyMethod()
                     .AllowAnyHeader()));
+
+            ApplicationContainer = WebApiDependencyConfiguration.Configure(services);
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -41,6 +48,8 @@ namespace FstgWebApi
             app.UseCors("AllowAll");
 
             app.UseMvc();
+
+            applicationLifetime.ApplicationStopped.Register(() => ApplicationContainer.Dispose());
         }
     }
 }
