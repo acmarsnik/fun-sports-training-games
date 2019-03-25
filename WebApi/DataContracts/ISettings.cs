ccdc89d1cfc82a6d76ad91162004e6c1c227027e
Albert Marsnik <albertmarsnik@gmail.com>
2019-03-25 07:33:32 +0100

    added settings and ISettings per https://www.qappdesign.com/using-mongodb-with-net-core-webapi/

---
+++ WebApi/DataContracts/ISettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FstgWebApi.DataContracts
+{
+    public interface IScore
+    {
+        int Id { get; set; }
+        int UserId { get; set; }
+        int Value { get; set; }
+    }
+}
