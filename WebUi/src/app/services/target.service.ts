69973cd9c81e7577d38132dbcef55a4476ce65a4
albertmarsnik <albertmarsnik@gmail.com>
2020-08-17 14:40:16 +0200

    task/pluralsight-angular-cli-john-papa _ Fixed import errors, some formatting.

--- WebUi/src/app/services/target.service.ts
+++ WebUi/src/app/services/target.service.ts
@@ -2,31 +2,33 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { IScore } from '../models/score';
 
-const options = {headers: new HttpHeaders({'Content-Type': 'application/json' })};
+const options = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TargetService {
   target_scores: number[] = [0, 1];
   scoreUrl = environment.scoreUrl;
   allScoresUrl = environment.allScoresUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  addTarget(target_score: number){
+  addTarget(target_score: number) {
     console.log(target_score);
     console.log(this.scoreUrl);
-    const data = {UserId: 1, Value: target_score};
-    return this.http.post(this.scoreUrl, data, options)
-    .pipe();
+    const data = { UserId: 1, Value: target_score };
+    return this.http.post(this.scoreUrl, data, options).pipe();
   }
 
-  deleteTarget(index: number){
+  deleteTarget(index: number) {
     this.target_scores.splice(index, 1);
   }
 
-  getTargets(){
+  getTargets() {
     return this.target_scores;
   }
 
@@ -37,10 +39,7 @@ export class TargetService {
     );
   } */
 
-  getTargetScores(): Observable<Score[]> {
-    return this.http.get<Score[]>(this.allScoresUrl)
-    .pipe(
-    );
+  getTargetScores(): Observable<IScore[]> {
+    return this.http.get<IScore[]>(this.allScoresUrl).pipe();
   }
-
 }
